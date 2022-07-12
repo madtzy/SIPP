163552b807e39d33929a3c14725ada23977012de
bayu.elroy <bayubriyanelroy@gmail.com>
2022-07-12 21:25:52 +0700

    #Update : Dashboard

--- app/Http/Controllers/AdminDashboardController.php
+++ app/Http/Controllers/AdminDashboardController.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Penjualan;
+use App\Models\Produk;
+use App\Models\Buyer;
 
 use Illuminate\Http\Request;
 
@@ -8,8 +11,21 @@ class AdminDashboardController extends Controller
 {
     public function index()
     {
+        $penjualan = Penjualan::sum('jumlah');
+        $stok = Produk::sum('stok');
+        $penghasilan = Buyer::where('status','terima')->sum('total_bayar');
+        $diterima = Buyer::where('status','terima')->count();
+        $ditolak = Buyer::where('status','tolak')->count();
+        $diproses = Buyer::where('status','proses')->count();
+
         return view('admin.dashboard',[
-            'title' => 'Dashboard'
+            'title' => 'Dashboard',
+            'penjualan' => $penjualan,
+            'stok' => $stok,
+            'penghasilan' => number_format($penghasilan,2,',','.'),
+            'diterima' => $diterima,
+            'ditolak' => $ditolak,
+            'diproses' => $diproses
         ]);
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,4 +1,68 @@
 @extends('admin.layouts.main')
 @section('content')
-    <h1>Hello word</h1>
+    <h4 class="text-dark fw-bold mt-2"><i class='bx bx-sm bxs-dashboard me-3'></i>Dashboard </h4>
+    <div class="container mb-5">
+        @if(Auth::user()->is_admin==1)
+        <div class="row">
+            <div class="col">
+                <div class="card mb-1">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Penjualan</h5>
+                        <h1>{{ $penjualan }}</h1>
+                        <p class="card-text">Karung</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card mb-1">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Stok</h5>
+                        <h1>{{ $stok }}</h1>
+                        <p class="card-text">Karung</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card mb-1">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Penghasilan</h5>
+                        <h1>{{ $penghasilan }}</h1>
+                        <p class="card-text">Rupiah</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        @endif
+        <div class="row">
+            <div class="col">
+                <div class="card mb-1">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Penjualan Diproses</h5>
+                        <h1>{{ $diproses }}</h1>
+                        <p class="card-text">Pesanan</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card mb-1">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Penjualan Diterima</h5>
+                        <h1>{{ $diterima }}</h1>
+                        <p class="card-text">Pesanan</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card mb-1">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Penjualan Ditolak</h5>
+                        <h1>{{ $ditolak }}</h1>
+                        <p class="card-text">Pesanan</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
 @endsection
